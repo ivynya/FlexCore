3a88c44b920f7ae88aae272f25aba77a511ba8a9
Kyle <sdbagel@gmail.com>
2020-07-29 19:17:58 -0700

    Modify build algorithm

--- docs/builder/builder.js
+++ docs/builder/builder.js
@@ -1,19 +1,23 @@
 var buildDict = {
-  "core-base": "@use './core/base';",
-  "core-alignment": "@use './core/alignment';",
-  "mod-container": "@use './modules/container';",
-  "mod-flexlist": "@use './modules/flexlist';",
-  "mod-navigation": "@use './modules/navigation';",
-  "comp-buttons": "@use './components/buttons';",
-  "comp-card": "@use './components/card';"
+  "core-base": "builder/core/_base.scss",
+  "core-alignment": "builder/core/_alignment.scss",
+  "mod-container": "builder/modules/_container.scss",
+  "mod-flexlist": "builder/modules/_flexlist.scss",
+  "mod-navigation": "builder/modules/_navigation.scss",
+  "comp-buttons": "builder/components/_buttons.scss",
+  "comp-card": "builder/components/_card.scss"
 }
 
 function mapper() {
   let returnString = "";
   for (const key in buildDict) {
     let el = document.getElementById(key);
-    if (el && el.checked)
-      returnString += buildDict[key];
+    if (el && el.checked) {
+      fetch(buildDict[key])
+      .then(res => res.text())
+      .then(res => returnString += res)
+      .catch(ex => console.log(ex));
+    }
   }
 
   console.log(`Build string: ${returnString}`);
